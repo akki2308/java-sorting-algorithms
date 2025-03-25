1d34b87a79a733afa7b370caf59cfcf6f77f4674
Akki <chauhanakshit61@gmail.com>
2025-03-25 10:20:16 +0530

    [Akshit] ADD: implementing Insertion  Sort

---
+++ java-sorting-algorithms/src/Insertion_Sort.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Insertion_Sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input for the number of employees
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        int[] employeeIDs = new int[n];
+
+        // Taking input for employee IDs
+        inputEmployeeIDs(sc, employeeIDs, n);
+
+        // Performing Insertion Sort
+        insertionSort(employeeIDs, n);
+
+        // Displaying sorted employee IDs
+        displaySortedEmployeeIDs(employeeIDs);
+
+        sc.close();
+    }
+
+    public static void inputEmployeeIDs(Scanner sc, int[] employeeIDs, int n) {
+        System.out.println("Enter the Employee IDs: ");
+        for (int i = 0; i < n; i++) {
+            employeeIDs[i] = sc.nextInt();
+        }
+    }
+
+    public static void insertionSort(int[] employeeIDs, int n) {
+        for (int i = 1; i < n; i++) {
+            int key = employeeIDs[i];
+            int j = i - 1;
+
+            // Moving elements that are greater than key one position ahead
+            while (j >= 0 && employeeIDs[j] > key) {
+                employeeIDs[j + 1] = employeeIDs[j];
+                j--;
+            }
+            employeeIDs[j + 1] = key;
+        }
+    }
+
+    public static void displaySortedEmployeeIDs(int[] employeeIDs) {
+        System.out.println("Sorted Employee IDs in ascending order: ");
+        for (int id : employeeIDs) {
+            System.out.print(id + " ");
+        }
+        System.out.println();
+    }
+}
+
